e4bdd7c87b0eb2b9938713cf21777a2993d51949
Alvaro Garcia <derecerca@gmail.com>
2017-07-11 00:30:18 +0200

    Green: support multiple 9s in a single input

--- src/test/java/com/example/kata/test/NumerologyShould.java
+++ src/test/java/com/example/kata/test/NumerologyShould.java
@@ -9,7 +9,12 @@ import static org.hamcrest.MatcherAssert.*;
 
 public class NumerologyShould {
     @Test
-    public void replace_a_nine_by_two_tens() {
+    public void replace_a_single_nine_by_two_tens() {
         assertThat(new Numerology().replace(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)), org.hamcrest.Matchers.is(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 10)));
+    }
+
+    @Test
+    public void replace_multiple_nines_by_two_tens_each() {assertThat(new Numerology().replace(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)), org.hamcrest.Matchers.is(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 10)));
+        assertThat(new Numerology().replace(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 9)), org.hamcrest.Matchers.is(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 10, 10)));
     }
 }
