b49a4eeba7941dc62f42a142c0979c2142156deb
Alvaro Garcia <derecerca@gmail.com>
2017-07-11 21:07:34 +0200

    Refactor: make the test function more generalized

--- src/test/java/com/example/kata/test/NumerologyShould.java
+++ src/test/java/com/example/kata/test/NumerologyShould.java
@@ -3,7 +3,6 @@ package com.example.kata.test;
 import com.example.kata.numerology.Numerology;
 import org.junit.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static java.util.Arrays.*;
@@ -13,21 +12,21 @@ import static org.hamcrest.Matchers.is;
 public class NumerologyShould {
     @Test
     public void replace_a_single_nine_by_two_tens() {
-        assertThat(replace(), is(asList(1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 10)));
+        assertThat(replace(asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)), is(asList(1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 10)));
     }
 
     @Test
     public void replace_multiple_nines_by_two_tens_each() {
-        assertThat(replace(), is(asList(1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 10)));
-        assertThat(new Numerology().replace(asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 9)), is(asList(1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 10, 10)));
+        assertThat(replace(asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)), is(asList(1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 10)));
+        assertThat(replace(asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 9)), is(asList(1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 10, 10)));
     }
 
     @Test
     public void replace_no_nines_by_two_tens_each() {
-        assertThat(new Numerology().replace(asList(1, 2, 3, 4, 5, 6, 7, 8)), is(asList(1, 2, 3, 4, 5, 6, 7, 8)));
+        assertThat(replace(asList(1, 2, 3, 4, 5, 6, 7, 8)), is(asList(1, 2, 3, 4, 5, 6, 7, 8)));
     }
 
-    private List<Integer> replace() {
-        return new Numerology().replace(asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+    private List<Integer> replace(List<Integer> input) {
+        return new Numerology().replace(input);
     }
 }
