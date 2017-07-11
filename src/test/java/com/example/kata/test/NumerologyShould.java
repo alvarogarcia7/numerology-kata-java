807c38c21cad047bda9737302dc1dd9fc9adde08
Alvaro Garcia <derecerca@gmail.com>
2017-07-11 21:25:36 +0200

    Refactor: make space for next feature

--- src/test/java/com/example/kata/test/NumerologyShould.java
+++ src/test/java/com/example/kata/test/NumerologyShould.java
@@ -5,31 +5,31 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static java.util.Arrays.*;
+import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class NumerologyShould {
     @Test
     public void replace_a_single_nine_by_two_tens() {
-        assertThat(replace(asList(1, 3, 4, 5, 6, 7, 8, 9, 10)), is(asList(1, 3, 4, 5, 6, 7, 8, 10, 10, 10)));
+        assertThat(replace(asList(1, 3, 4, 5, 7, 8, 9, 10)), is(asList(1, 3, 4, 5, 7, 8, 10, 10, 10)));
     }
 
     @Test
     public void replace_multiple_nines_by_two_tens_each() {
-        assertThat(replace(asList(1, 3, 4, 5, 6, 7, 8, 9, 10)), is(asList(1, 3, 4, 5, 6, 7, 8, 10, 10, 10)));
-        assertThat(replace(asList(1, 3, 4, 5, 6, 7, 8, 9, 9)), is(asList(1, 3, 4, 5, 6, 7, 8, 10, 10, 10, 10)));
+        assertThat(replace(asList(1, 3, 4, 5, 7, 8, 9, 10)), is(asList(1, 3, 4, 5, 7, 8, 10, 10, 10)));
+        assertThat(replace(asList(1, 3, 4, 5, 7, 8, 9, 9)), is(asList(1, 3, 4, 5, 7, 8, 10, 10, 10, 10)));
     }
 
     @Test
     public void replace_no_nines_by_two_tens_each() {
-        assertThat(replace(asList(1, 3, 4, 5, 6, 7, 8)), is(asList(1, 3, 4, 5, 6, 7, 8)));
+        assertThat(replace(asList(1, 3, 4, 5, 7, 8)), is(asList(1, 3, 4, 5, 7, 8)));
     }
 
 
     @Test
     public void replace_the_number_previous_to_a_2_for_as_many_ones() {
-        assertThat(replace(asList(8, 2)), is(asList(8, 1,1,1,1,1,1,1,1)));
+        assertThat(replace(asList(8, 2)), is(asList(8, 1, 1, 1, 1, 1, 1, 1, 1)));
     }
 
     @Test
