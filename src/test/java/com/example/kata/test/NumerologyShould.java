3ba9e43be15be7b667695e940c265144af1c7a73
Alvaro Garcia <derecerca@gmail.com>
2017-07-11 21:04:14 +0200

    Green: replace multiple nines

--- src/test/java/com/example/kata/test/NumerologyShould.java
+++ src/test/java/com/example/kata/test/NumerologyShould.java
@@ -4,17 +4,25 @@ import com.example.kata.numerology.Numerology;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
-import static org.hamcrest.MatcherAssert.*;
+import static java.util.Arrays.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 public class NumerologyShould {
     @Test
     public void replace_a_single_nine_by_two_tens() {
-        assertThat(new Numerology().replace(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)), org.hamcrest.Matchers.is(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 10)));
+        assertThat(replace(), is(asList(1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 10)));
     }
 
     @Test
-    public void replace_multiple_nines_by_two_tens_each() {assertThat(new Numerology().replace(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)), org.hamcrest.Matchers.is(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 10)));
-        assertThat(new Numerology().replace(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 9)), org.hamcrest.Matchers.is(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 10, 10)));
+    public void replace_multiple_nines_by_two_tens_each() {
+        assertThat(replace(), is(asList(1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 10)));
+        assertThat(new Numerology().replace(asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 9)), is(asList(1, 2, 3, 4, 5, 6, 7, 8, 10, 10, 10, 10)));
+    }
+
+    private List<Integer> replace() {
+        return new Numerology().replace(asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
     }
 }
