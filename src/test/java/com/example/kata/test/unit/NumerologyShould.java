ab91a02d596c94634da0ef4c51651b7d1d0ff7d4
Alvaro Garcia <derecerca@gmail.com>
2017-07-12 20:51:36 +0200

    Refactor: turn the unit test into real unit tests

--- src/test/java/com/example/kata/test/unit/NumerologyShould.java
+++ src/test/java/com/example/kata/test/unit/NumerologyShould.java
@@ -14,54 +14,72 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class NumerologyShould {
+
+    //Rule number 1
     @Test
     public void replace_a_single_nine_by_two_tens() {
-        assertThat(replace(asList(1, 3, 4, 5, 7, 8, 9, 10)), is(asList(1, 3, 4, 5, 7, 8, 10, 10, 10)));
+        assertThat(replaceOnlyWithFirstRule(asList(1, 3, 4, 5, 7, 8, 9, 10)), is(asList(1, 3, 4, 5, 7, 8, 10, 10, 10)));
     }
 
     @Test
     public void replace_multiple_nines_by_two_tens_each() {
-        assertThat(replace(asList(1, 3, 4, 5, 7, 8, 9, 10)), is(asList(1, 3, 4, 5, 7, 8, 10, 10, 10)));
-        assertThat(replace(asList(1, 3, 4, 5, 7, 8, 9, 9)), is(asList(1, 3, 4, 5, 7, 8, 10, 10, 10, 10)));
+        assertThat(replaceOnlyWithFirstRule(asList(1, 3, 4, 5, 7, 8, 9, 10)), is(asList(1, 3, 4, 5, 7, 8, 10, 10, 10)));
+        assertThat(replaceOnlyWithFirstRule(asList(1, 3, 4, 5, 7, 8, 9, 9)), is(asList(1, 3, 4, 5, 7, 8, 10, 10, 10, 10)));
     }
 
     @Test
     public void replace_no_nines_by_two_tens_each() {
-        assertThat(replace(asList(1, 3, 4, 5, 7, 8)), is(asList(1, 3, 4, 5, 7, 8)));
+        assertThat(replaceOnlyWithFirstRule(asList(1, 3, 4, 5, 7, 8)), is(asList(1, 3, 4, 5, 7, 8)));
     }
 
 
+    //Rule number 2
     @Test
     public void replace_the_number_previous_to_a_2_for_as_many_ones() {
-        assertThat(replace(asList(8, 2)), is(asList(8, 1, 1, 1, 1, 1, 1, 1, 1)));
+        assertThat(replaceOnlyWithSecondRule(asList(8, 2)), is(asList(8, 1, 1, 1, 1, 1, 1, 1, 1)));
     }
 
     @Test
     public void replace_the_number_previous_to_a_2_for_as_many_ones_but_its_not_possible() {
-        assertThat(replace(asList(2)), is(asList(2)));
+        assertThat(replaceOnlyWithSecondRule(asList(2)), is(asList(2)));
     }
 
-
+    //Rule number 3
     @Test
     public void replace_rule_3() {
-        assertThat(replace(asList(1, 6, 3, 4, 5)), is(asList(1, 3, 3, 3, 3, 4, 5)));
+        assertThat(replaceOnlyWithThirdRule(asList(1, 6, 3, 4, 5)), is(asList(1, 3, 3, 3, 3, 4, 5)));
     }
 
     @Test
     public void replace_rule_3_cant_because_of_the_left() {
-        assertThat(replace(asList(6, 3, 4, 5)), is(asList(6, 3, 4, 5)));
+        assertThat(replaceOnlyWithThirdRule(asList(6, 3, 4, 5)), is(asList(6, 3, 4, 5)));
     }
 
     @Test
     public void replace_rule_3_cant_because_of_the_right() {
-        assertThat(replace(asList(4, 6, 3, 4, 5)), is(asList(4, 6, 3, 4, 5)));
-        assertThat(replace(asList(1, 6)), is(asList(1, 6)));
+        assertThat(replaceOnlyWithThirdRule(asList(4, 6, 3, 4, 5)), is(asList(4, 6, 3, 4, 5)));
+        assertThat(replaceOnlyWithThirdRule(asList(1, 6)), is(asList(1, 6)));
     }
 
-    private List<Integer> replace(List<Integer> input) {
+    private List<Integer> replaceOnlyWithSecondRule(List<Integer> input) {
+        return new Numerology(
+                new RuleReplace2ForAnEqualAmountOfNumbersToTheLeft(),
+                new RuleIdentity())
+
+                .replace(input);
+    }
+
+    private List<Integer> replaceOnlyWithFirstRule(List<Integer> input) {
         return new Numerology(
                 new Rule9ForTwo10s(),
-                new RuleReplace2ForAnEqualAmountOfNumbersToTheLeft(),
+                new RuleIdentity())
+
+                .replace(input);
+    }
+
+
+    private List<Integer> replaceOnlyWithThirdRule(List<Integer> input) {
+        return new Numerology(
                 new RuleReplace6ForAsMany3AsTheValueToTheNthRight(),
                 new RuleIdentity())
 
