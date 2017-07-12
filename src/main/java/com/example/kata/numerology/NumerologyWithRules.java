d8fe8e7b1f120a9a7a29ddddcf7a9a127101d5b2
Alvaro Garcia <derecerca@gmail.com>
2017-07-12 20:01:29 +0200

    Refactor: accept multiple rules

--- src/main/java/com/example/kata/numerology/NumerologyWithRules.java
+++ src/main/java/com/example/kata/numerology/NumerologyWithRules.java
@@ -1,17 +1,16 @@
 package com.example.kata.numerology;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.IntStream;
 
 public class NumerologyWithRules {
 
-    private Rule rule;
-    private Rule rule2;
+    private final Rule[] rules;
 
-    public NumerologyWithRules(Rule rule, Rule rule2) {
-        this.rule = rule;
-        this.rule2 = rule2;
+    public NumerologyWithRules(Rule... rules) {
+        this.rules = rules;
     }
 
     public List<Integer> replace(List<Integer> input) {
@@ -19,8 +18,8 @@ public class NumerologyWithRules {
         List<Integer> result = new ArrayList<>();
         for (int i = 0; elements.exists(i); i++) {
             Integer integer = elements.at(i);
-            rule2.apply(elements, i).map(result::addAll);
-            rule.apply(elements, i).map(result::addAll);
+            final Integer index = i;
+            Arrays.stream(rules).forEachOrdered(rule -> rule.apply(elements, index).map(result::addAll));
             if (integer.equals(9)) {
             } else if (integer.equals(2) && elements.exists(i - 1)) {
                 //can't remove this yet
