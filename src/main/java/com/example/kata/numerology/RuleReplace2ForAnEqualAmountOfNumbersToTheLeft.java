5420cbb33c40bac36b2f495c1e5a5e5a2bb55336
Alvaro Garcia <derecerca@gmail.com>
2017-07-12 19:53:54 +0200

    Refactor: remove dead code; inline function

--- src/main/java/com/example/kata/numerology/RuleReplace2ForAnEqualAmountOfNumbersToTheLeft.java
+++ src/main/java/com/example/kata/numerology/RuleReplace2ForAnEqualAmountOfNumbersToTheLeft.java
@@ -7,19 +7,12 @@ import java.util.stream.IntStream;
 import static java.util.stream.Collectors.toList;
 
 public class RuleReplace2ForAnEqualAmountOfNumbersToTheLeft {
-    void applyRule2(Elements elements, int i, List<Integer> result) {
-        x(elements, i).forEach(x1->result.add(x1));
-    }
-
-    private List<Integer> x(Elements elements, int i) {
-        Integer previousNumber = elements.previousOf(i);
-        return IntStream.rangeClosed(1, previousNumber).map((x) -> 1).boxed().collect(toList());
-    }
 
     public Optional<List<Integer>> apply(Elements elements, int index) {
         Integer integer = elements.at(index);
         if (integer.equals(2) && elements.exists(index - 1)) {
-            return Optional.of(x(elements, index));
+            Integer previousNumber = elements.previousOf(index);
+            return Optional.of(IntStream.rangeClosed(1, previousNumber).map((x) -> 1).boxed().collect(toList()));
         }
         return Optional.empty();
     }
