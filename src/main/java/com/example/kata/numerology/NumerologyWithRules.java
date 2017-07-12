e8dc40d62256f086042862952ebe39b19b689e53
Alvaro Garcia <derecerca@gmail.com>
2017-07-12 19:48:18 +0200

    Refactor: the apply method removes the temporal coupling between the query and the command

--- src/main/java/com/example/kata/numerology/NumerologyWithRules.java
+++ src/main/java/com/example/kata/numerology/NumerologyWithRules.java
@@ -24,7 +24,7 @@ public class NumerologyWithRules {
             if (integer.equals(9)) {
                 result.addAll(this.rule9ForTwo10s.apply(elements));
             } else if (integer.equals(2) && elements.exists(i - 1)) {
-                rule2.applyRule2(elements, i, result);
+                result.addAll(rule2.apply(elements, i).get());
             } else if (integer.equals(6) && (elements.exists(i - 1)) && elementExists(input, i + previous(input, i))) {
                 Integer previous = elements.previousOf(i);
                 int numberOfTimes = elements.at(previous + i);
--- src/main/java/com/example/kata/numerology/RuleReplace2ForAnEqualAmountOfNumbersToTheLeft.java
+++ src/main/java/com/example/kata/numerology/RuleReplace2ForAnEqualAmountOfNumbersToTheLeft.java
@@ -1,11 +1,26 @@
 package com.example.kata.numerology;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
 
 public class RuleReplace2ForAnEqualAmountOfNumbersToTheLeft {
     void applyRule2(Elements elements, int i, List<Integer> result) {
+        x(elements, i).forEach(x1->result.add(x1));
+    }
+
+    private List<Integer> x(Elements elements, int i) {
         Integer previousNumber = elements.previousOf(i);
-        IntStream.rangeClosed(1, previousNumber).forEach((x) -> result.add(1));
+        return IntStream.rangeClosed(1, previousNumber).map((x) -> 1).boxed().collect(toList());
+    }
+
+    public Optional<List<Integer>> apply(Elements elements, int index) {
+        Integer integer = elements.at(index);
+        if (integer.equals(2) && elements.exists(index - 1)) {
+            return Optional.of(x(elements, index));
+        }
+        return Optional.empty();
     }
 }
