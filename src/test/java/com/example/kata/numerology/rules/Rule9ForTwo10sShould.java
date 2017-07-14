54e1bf85aa38745106585246b65dcd3115eccd49
Alvaro Garcia <derecerca@gmail.com>
2017-07-14 06:44:18 +0200

    Refactor: move tests for rule1 to its own file

---
+++ src/test/java/com/example/kata/numerology/rules/Rule9ForTwo10sShould.java
@@ -0,0 +1,30 @@
+package com.example.kata.numerology.rules;
+
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static com.example.kata.test.unit.RuleCaseBuilder.apply;
+import static com.example.kata.test.unit.Rules.rule1;
+import static java.util.Arrays.asList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class Rule9ForTwo10sShould {
+
+    @Test
+    public void rule_1_cannot_access_a_nonexisting_index() {
+        assertThat(apply(rule1()).to(asList(2)).at(1), is(Optional.empty()));
+    }
+
+    @Test
+    public void replace_a_single_nine_by_two_tens() {
+        assertThat(apply(rule1()).to(asList(9)).at(0), is(Optional.of(asList(10, 10))));
+    }
+
+    @Test
+    public void replace_no_nines_by_two_tens_each() {
+        assertThat(apply(rule1()).to(asList(1)).at(0), is(Optional.empty()));
+    }
+
+}
--- src/test/java/com/example/kata/test/unit/NumerologyShould.java
+++ src/test/java/com/example/kata/test/unit/NumerologyShould.java
@@ -12,22 +12,6 @@ import static org.hamcrest.Matchers.is;
 
 public class NumerologyShould {
 
-    //Rule number 1
-    @Test
-    public void rule_1_cannot_access_a_nonexisting_index() {
-        assertThat(apply(rule1()).to(asList(2)).at(1), is(Optional.empty()));
-    }
-
-    @Test
-    public void replace_a_single_nine_by_two_tens() {
-        assertThat(apply(rule1()).to(asList(9)).at(0), is(Optional.of(asList(10, 10))));
-    }
-
-    @Test
-    public void replace_no_nines_by_two_tens_each() {
-        assertThat(apply(rule1()).to(asList(1)).at(0), is(Optional.empty()));
-    }
-
     //Rule number 2
     @Test
     public void replace_the_number_previous_to_a_2_for_as_many_ones() {
