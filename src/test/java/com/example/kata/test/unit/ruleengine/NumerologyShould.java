786d1047fd01fe99cf37e210c38ce8b1bd8af783
Alvaro Garcia <derecerca@gmail.com>
2017-07-14 07:51:57 +0200

    Refactor: inline function

--- src/test/java/com/example/kata/test/unit/ruleengine/NumerologyShould.java
+++ src/test/java/com/example/kata/test/unit/ruleengine/NumerologyShould.java
@@ -17,7 +17,6 @@ public class NumerologyShould {
 
     @Test
     public void apply_only_the_first_rule_that_matches() {
-
         Rule tautology1 = tautologyThatProduces(asList(1));
         Rule tautology2 = tautologyThatProduces(asList(2));
         Numerology numerology = new Numerology(tautology1, tautology2);
@@ -29,9 +28,7 @@ public class NumerologyShould {
 
     @Test
     public void not_depend_on_having_a_catchall_rule() {
-
-        Rule contradiction = contradiction();
-        Numerology numerology = new Numerology(contradiction);
+        Numerology numerology = new Numerology(contradiction());
 
         List<Integer> result = numerology.replace(asList(9));
 
