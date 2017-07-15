b61580c4586437522f4ad5d1493dff43c3e17494
Alvaro Garcia <derecerca@gmail.com>
2017-07-15 13:00:04 +0200

    Prefer having the names (levels of abstraction)
    Revert "Refactor: inline all variables to achieve a 1-statement lambda (no {})"
    
    This reverts commit 40e45c07a6028a023dbe01b469c092b96d6d7997.

--- src/main/java/com/example/kata/numerology/rules/RuleReplace6ForAsMany3AsTheValueToTheNthRight.java
+++ src/main/java/com/example/kata/numerology/rules/RuleReplace6ForAsMany3AsTheValueToTheNthRight.java
@@ -20,12 +20,17 @@ public class RuleReplace6ForAsMany3AsTheValueToTheNthRight implements Rule {
     public Option<List<Integer>> apply(Elements elements, int i) {
         return withinBounds
                 .apply(elements, i)
-                .flatMap(r -> equalityTo.apply(elements, i)
+                .flatMap(r -> equalityTo
+                        .apply(elements, i)
                         .flatMap(r2 -> hasPrevious.apply(elements, i)
                                 .flatMap(r3b -> withinBounds.apply(elements, i - 1))
                                 .flatMap(r3 -> withinBounds.apply(elements, elements.at(i - 1)))
                                 .flatMap(r3 -> withinBounds.apply(elements, i + elements.at(i - 1)))
                                 .flatMap(r3 -> HasPreviousElementRule.atADistanceOf(elements.at(i - 1)).apply(elements, i).flatMap(r4 ->
-                                        Option.of(IntStream.rangeClosed(1, elements.at(elements.previousOf(i) + i)).map((x) -> 3).boxed().collect(toList()))))));
+                                {
+                                    Integer previous = elements.previousOf(i);
+                                    int numberOfTimes = elements.at(previous + i);
+                                    return Option.of(IntStream.rangeClosed(1, numberOfTimes).map((x) -> 3).boxed().collect(toList()));
+                                }))));
     }
 }
