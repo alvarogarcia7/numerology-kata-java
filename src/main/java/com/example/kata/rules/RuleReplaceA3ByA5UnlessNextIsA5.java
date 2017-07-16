42d2dd32dc38a0ffe81fc865416192c0c36f504b
Alvaro Garcia <derecerca@gmail.com>
2017-07-16 17:07:29 +0200

    Green: replace a 3 by a 5 unless the next is a 5 - happy path

--- src/main/java/com/example/kata/rules/RuleReplaceA3ByA5UnlessNextIsA5.java
+++ src/main/java/com/example/kata/rules/RuleReplaceA3ByA5UnlessNextIsA5.java
@@ -2,16 +2,21 @@ package com.example.kata.rules;
 
 import com.example.kata.numerology.Elements;
 import com.example.kata.numerology.rules.Rule;
+import com.example.kata.numerology.selectors.AtADistanceSelector;
 import io.vavr.control.Option;
 
 import java.util.List;
 
+import static io.vavr.control.Option.none;
 import static io.vavr.control.Option.of;
 import static java.util.Arrays.asList;
 
 public class RuleReplaceA3ByA5UnlessNextIsA5 implements Rule {
     @Override
     public Option<List<Integer>> apply(Elements elements, int index) {
+        if (elements.at(index + 1).equals(5)) {
+            return none();
+        }
         return of(asList(5));
     }
 }
--- src/test/java/com/example/kata/test/unit/rule/RuleReplaceA3ByA5UnlessNextIsA5Should.java
+++ src/test/java/com/example/kata/test/unit/rule/RuleReplaceA3ByA5UnlessNextIsA5Should.java
@@ -6,6 +6,7 @@ import io.vavr.control.Option;
 import org.junit.Test;
 
 import static com.example.kata.test.helper.RuleCaseBuilder.apply;
+import static io.vavr.control.Option.none;
 import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
@@ -15,6 +16,11 @@ public class RuleReplaceA3ByA5UnlessNextIsA5Should {
     @Test
     public void happy_path() {
         assertThat(apply(rule()).to(asList(3, 4)).at(0), is(Option.of(asList(5))));
+    }
+
+    @Test
+    public void no_replace_as_next_is_a_5() {
+        assertThat(apply(rule()).to(asList(3, 5)).at(0), is(Option.none()));
     }
 
     private Rule rule() {
