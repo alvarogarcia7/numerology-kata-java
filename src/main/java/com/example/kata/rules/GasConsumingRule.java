1b41fe43d92ad3eebf9e60c9333b41534a46142c
Alvaro Garcia <derecerca@gmail.com>
2017-07-16 19:44:14 +0200

    Refactor: push the gas consuming scheme to RefuelingScheme

--- src/main/java/com/example/kata/rules/GasConsumingRule.java
+++ src/main/java/com/example/kata/rules/GasConsumingRule.java
@@ -34,4 +34,19 @@ public class GasConsumingRule implements Rule {
         }
         return Option.none();
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GasConsumingRule that = (GasConsumingRule) o;
+
+        return rule != null ? rule.equals(that.rule) : that.rule == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return rule != null ? rule.hashCode() : 0;
+    }
 }
--- src/main/java/com/example/kata/rules/RulesThatEnableOtherRules.java
+++ src/main/java/com/example/kata/rules/RulesThatEnableOtherRules.java
@@ -5,14 +5,26 @@ import com.example.kata.numerology.rules.Rule;
 import io.vavr.control.Option;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class RulesThatEnableOtherRules implements Rule {
     private final GasConsumingRule gasConsumingRule1;
     private final GasConsumingRule gasConsumingRule2;
+    private final RefuelingScheme refuelingScheme;
 
     public RulesThatEnableOtherRules(Rule rule1, Rule rule2) {
         gasConsumingRule1 = new GasConsumingRule(rule1, 1);
         gasConsumingRule2 = new GasConsumingRule(rule2, 1);
+        Consumer<GasConsumingRule> gasConsumingRuleGasConsumingRuleFunction = gasConsumingRule -> {
+            if(gasConsumingRule2.equals(gasConsumingRule)){
+                gasConsumingRule2.consumeGas();
+                gasConsumingRule1.refuel();
+            } else if(gasConsumingRule1.equals(gasConsumingRule)) {
+                gasConsumingRule2.refuel();
+                gasConsumingRule1.consumeGas();
+            }
+        };
+        refuelingScheme = new RefuelingScheme(gasConsumingRuleGasConsumingRuleFunction);
     }
 
     @Override
@@ -20,13 +32,15 @@ public class RulesThatEnableOtherRules implements Rule {
         //calculate state of rules until now
         for (int i = 0; i < index; i++) {
             if (gasConsumingRule1.apply(elements, i).isDefined()) {
-                gasConsumingRule1.consumeGas();
-                gasConsumingRule2.refuel();
+                refuelingScheme.apply(gasConsumingRule1);
+//                gasConsumingRule1.consumeGas();
+//                gasConsumingRule2.refuel();
                 continue;
             }
             if (gasConsumingRule2.apply(elements, i).isDefined()) {
-                gasConsumingRule1.refuel();
-                gasConsumingRule2.consumeGas();
+                refuelingScheme.apply(gasConsumingRule2);
+//                gasConsumingRule1.refuel();
+//                gasConsumingRule2.consumeGas();
                 continue;
             }
         }
@@ -35,4 +49,15 @@ public class RulesThatEnableOtherRules implements Rule {
                 .orElse(gasConsumingRule2.apply(elements, index));
     }
 
+    private class RefuelingScheme {
+        private final Consumer<GasConsumingRule> scheme;
+
+        public RefuelingScheme(Consumer<GasConsumingRule> scheme) {
+            this.scheme = scheme;
+        }
+
+        public void apply(GasConsumingRule gasConsumingRule) {
+            this.scheme.accept(gasConsumingRule);
+        }
+    }
 }
